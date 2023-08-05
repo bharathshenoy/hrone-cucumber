d70be5b39830c23d40c38b0cc35a417842692da3
Bharath Shenoy <shenoyg1@gmail.com>
2023-08-05 18:30:37 +0530

    Updated runner class

--- src/test/java/testRunner/hrOneTest.java
+++ src/test/java/testRunner/hrOneTest.java
@@ -7,7 +7,7 @@ import io.cucumber.testng.CucumberOptions;
         features = {"src/test/resources/features/login.feature"},
         glue = {"steps", "hooks"},
         plugin = {"pretty",
-                "html:target/MyReport/cucumber",
+                "html:target/MyReport/cucumber.html",
                 "json:target/MyReport/report.json",
                 "junit:target/MyReport/report.xml",
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
